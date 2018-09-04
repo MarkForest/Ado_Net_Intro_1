a59f1d82a657665b604f67c4db4cf52d46ca261d
markforest <toxerius7@gmail.com>
2018-09-04 15:24:50 +0300

    Параметрезированые запросы

---
+++ AdoNetIntroDemo1/AdoNetIntroDemo1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace AdoNetIntroDemo1
+{
+    class Program
+    {
+        static SqlConnection sqlConnection = null;
+        static string connectionString;
+        static SqlCommand sqlCommand = null;
+        static SqlDataReader sqlDataReader = null;
+        static void Main(string[] args)
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            //InsertAuthor();
+            SelectAuthor();
+            
+            Console.ReadKey();
+            
+        }
+
+        private static void SelectAuthor()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+
+            string querySelect = "select * from books; select * from Author;";
+            int line = 0;
+           
+
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(querySelect, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                do
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                Console.Write(sqlDataReader.GetName(i) + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                        line++;
+                        Console.WriteLine(sqlDataReader[0] + " " + sqlDataReader[1] + " " + sqlDataReader[2]+ " "+ sqlDataReader[3] + " ");
+                    }
+                } while (sqlDataReader.NextResult());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection?.Close();
+                sqlDataReader?.Close();
+            }
+
+        }
+
+        private static void InsertAuthor()
+        {
+            
+            //1....
+            //sqlConnection = new SqlConnection();
+            //sqlConnection.ConnectionString = connectionString;
+            //2....
+            sqlConnection = new SqlConnection(connectionString);
+            string query = "insert into Author values('Roger', 'Zelazny')";
+           
+            //sqlCommand = new SqlCommand();
+            //sqlCommand.Connection = sqlConnection;
+            //sqlCommand.CommandText = query;
+            sqlCommand = new SqlCommand(query, sqlConnection);
+
+
+            try
+            {
+                sqlConnection.Open();
+                int rows = sqlCommand.ExecuteNonQuery();
+                Console.WriteLine($"Count working rows = {rows}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //if (sqlConnection != null) {
+                //  sqlConnection.Close();
+                //}
+                sqlConnection?.Close();
+            }
+        }
+    }
+}
---
+++ AdoNetIntroDemo1/ParametrizidQueryDemo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParametrizidQueryDemo
+{
+    public partial class Form1 : Form
+    {
+        
+        string connectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+        SqlConnection sqlConnection = null;
+        SqlDataReader sqlDataReader = null;
+        SqlCommand sqlCommand = null;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length <= 50 && textBox2.Text.Length <= 50) {
+                sqlConnection = new SqlConnection(connectionString);
+                string query = "insert into Author values(@firstName, @lastName)";
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlParameter param2 = new SqlParameter();
+                param2.ParameterName = "@lastName";
+                param2.SqlDbType = SqlDbType.VarChar;
+                param2.Value = textBox2.Text;
+                sqlCommand.Parameters.Add(param2);
+                //sqlCommand.Parameters.Add("@firstName", SqlDbType.VarChar).Value = textBox1.Text;
+                sqlCommand.Parameters.AddWithValue("@firstName", textBox1.Text);
+
+
+                try
+                {
+                    sqlConnection.Open();
+                    int rows = sqlCommand.ExecuteNonQuery();
+                    MessageBox.Show($"Count working rows = {rows}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlConnection?.Close();
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Не тупи пользователь");
+            }
+
+        }
+    }
+}
